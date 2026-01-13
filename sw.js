4599b06fa8e45a0a15cb7f51c8453b84529d751f
Gonza-wf <121890733+Gonza-wf@users.noreply.github.com>
2026-01-13 00:31:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("battery-baja").then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./game.js"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
