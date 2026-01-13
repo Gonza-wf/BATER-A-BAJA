92e7f19fcc8a2bd6447a74771fc929553ba70016
Gonza-wf <121890733+Gonza-wf@users.noreply.github.com>
2026-01-13 00:41:34 -0300

    Update service worker cache version to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bateria-baja-v2";
+const CACHE_NAME = "bateria-baja-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -30,4 +30,5 @@ self.addEventListener("fetch", event => {
     caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
+
 
